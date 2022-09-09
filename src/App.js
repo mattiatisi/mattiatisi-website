66a26a08b2dcfa897ee6cd5b12a89a41ab36c719
mattiatisi <92105136+mattiatisi@users.noreply.github.com>
2022-09-09 17:41:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>src/App.js</code> reload.
+           <code>src/App.js<
         </p>
         <a
           className="App-link"
@@ -15,7 +15,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
