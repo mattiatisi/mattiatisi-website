9b8df29327e85a93f437eb89c28fd31110a05bd4
mattiatisi <92105136+mattiatisi@users.noreply.github.com>
2022-09-09 18:10:41 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-Welcome        </p>
+        <p>HI </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-        </a>
+        ></a>
       </header>
     </div>
   );
